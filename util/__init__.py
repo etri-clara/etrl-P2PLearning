a82a9e35ad7037cc4d68e5cfd7e92e1f642ef14b
Yong-Ju Lee <yongju@etr.re.kr>
2022-10-28 16:02:51 +0900

    main function updates

--- util/__init__.py
+++ util/__init__.py
@@ -1,1 +1,2 @@
 from .trainer import *
+from .log import *
---
+++ util/log.py
@@ -0,0 +1,48 @@
+import logging
+from argparse import ArgumentParser
+from enum import IntEnum
+import os
+import os.path as path
+import sys
+
+LOGFORMAT = "[%(asctime)s] <%(levelname)s> %(message)s"
+
+class LogLevel(IntEnum):
+    DEBUG = 10
+    INFO = 20
+    WARNING = 30
+    ERROR = 40
+    CRITICAL = 50
+
+    @classmethod
+    def name_of(cls, name: str):
+        name = name.upper()
+        for e in cls:
+            if e.name == name:
+                return e
+        raise ValueError(f"Unknown {cls.__name__} name: {name}")
+
+    @classmethod
+    def value_of(cls, value: int):
+        for e in cls:
+            if e.value == value:
+                return e
+        raise ValueError(f"Unknown {cls.__name__} value: {value}")
+
+    def __str__(self):
+        return self.name.upper()
+
+
+def config_logger(logformat: str = LOGFORMAT,
+                  loglevel: LogLevel = LogLevel.INFO,
+                  logfile: str = None):
+
+    if logfile is None:
+        logging.basicConfig(level=loglevel, format=logformat,
+                            stream=sys.stderr)  # ログファイル未指定の場合、標準エラーに出力
+    else:
+        logdir = path.dirname(logfile)
+        os.makedirs(logdir, exist_ok=True)
+        logging.basicConfig(level=loglevel, format=logformat,
+                            filename=logfile)  # ログファイル未指定の場合、標準エラーに出力
+
